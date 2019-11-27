527996fa6c868e91a58783459fba590181bb9fd7
Gabriel <gabrielslolihhjj@gmail.com>
2019-11-27 16:44:10 -0300

    finalizar carrinho

--- view/botão.php
+++ view/botão.php
@@ -4,6 +4,7 @@
 </button>
 
 <!-- Modal -->
+
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
